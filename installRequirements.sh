93a54ce3a68e797e9c5ca7eff5a60158da2cf813
sigvaldr <sigvaldrnotthrafn@proton.me>
2026-05-27 02:48:19 -0400

    fixed req script

--- installRequirements.sh
+++ installRequirements.sh
@@ -4,7 +4,10 @@ echo "Installing Prereqs..."
 sudo pacman -S --needed xorg-xfd ttf-bigblueterminal-nerd alacritty starship rofi thunar eza
 echo ""
 echo "Installing PreReqs from AUR..."
-yay -S nordic-theme nordzy-icon-theme nordzy-cursors-theme librewolf-bin
+yay -S nordic-theme nordzy-icon-theme nordzy-cursors-theme librewolf-bin	
+echo ""
+echo "Switching to fish shell..."
+chsh -s /usr/bin/fish
 echo ""
 echo "Applying Default Themes & Configs..."
 cp -R .config ~/
