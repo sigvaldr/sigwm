614d38981131378354b26ef81f867cd0babc2371
sigvaldr <sigvaldrnotthrafn@proton.me>
2026-05-27 05:57:16 -0400

    rewrote install reqs

--- installRequirements.sh
+++ installRequirements.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
-
-echo "Installing Audio & Video..."
-sudo pacman -Sy
-sudo pacman -S xorg-server xorg-xinit intel-media-driver libva-intel-driver mesa vulkan-intel vulkan-nouveau vulkan-radeon xf86-video-amdgpu xf86-video-ati xf86-video-nouveau pipewire
-echo ""
+sig=$PWD
+clear
 echo "Installing Prereqs..."
-sudo pacman -S --needed git base-devel xorg-xfd ttf-bigblueterminal-nerd alacritty starship rofi thunar eza
+sudo pacman -S --needed git base-devel xorg-server xorg-xinit xorg-xfd ttf-bigblueterminal-nerd alacritty starship rofi thunar eza
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-echo ""
-echo "Switching to fish shell..."
-chsh -s /usr/bin/fish
-echo ""
+clear
 echo "Applying Default Themes & Configs..."
 cp -R .config ~/
 cp .xinitrc ~/
-echo ""
 mkdir -p ~/.src
+clear
 echo "Installing YaY"
-cd ~/src
+cd ~/.src
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-echo ""
+clear
 echo "Installing PreReqs from AUR..."
-yay -S nordic-theme nordzy-icon-theme nordzy-cursors-theme librewolf-bin light
-echo ""
+yay -S nordic-theme nordzy-icon-theme nordzy-cursors-theme librewolf-bin light fish
+clear
+echo "Switching to fish shell..."
+chsh -s /usr/bin/fish
+clear
 echo "Installing extras..."
 cd ~/.src
+source ~/.bashrc
 echo "Building rdir..."
 git clone https://github.com/sigvaldr/rdir.git
 cd rdir
 cargo build --release
 echo "Installing rdir..."
 sudo cp target/release/rdir /usr/bin/
-echo ""
+clear
 echo "Buildilng boxr..."
 cd ~/.src
 git clone https://github.com/sigvaldr/boxr.git
@@ -42,6 +40,15 @@ cargo build --release
 echo "Installing boxr..."
 sudo cp target/release/boxr /usr/bin
 sudo cp target/release/unboxr /usr/bin
-echo ""
-echo "-------------"
-echo "Requirements installed. You may now use the 'startx' command to start sigwm"
+clear
+echo "  Requirements installed"
+sudo usermod -aG audio $USER
+sudo usermod -aG video $USER
+echo "Building SIGWM..."
+cd $sig
+cargo build --release
+sudo cp target/release/sigwm /usr/bin
+echo "-----------------------------------"
+echo "Installation complete."
+echo "Please reboot into your new WM :)"
+echo "-----------------------------------"
