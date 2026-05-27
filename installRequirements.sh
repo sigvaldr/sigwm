b7b21cffc36c7e1911f2bccc4999400c242b213c
sigvaldr <sigvaldrnotthrafn@proton.me>
2026-05-27 04:44:05 -0400

    req install script overhaul

--- installRequirements.sh
+++ installRequirements.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo "Installing Prereqs..."
-sudo pacman -S --needed xorg-xfd ttf-bigblueterminal-nerd alacritty starship rofi thunar eza
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo "Installing Audio & Video..."
+sudo pacman -Sy
+sudo pacman -S xorg-server xorg-xinit intel-media-driver libva-intel-driver mesa vulkan-intel vulkan-nouveau vulkan-radeon xf86-video-amdgpu xf86-video-ati xf86-video-nouveau pipewire
 echo ""
-echo "Installing PreReqs from AUR..."
-yay -S nordic-theme nordzy-icon-theme nordzy-cursors-theme librewolf-bin light
+echo "Installing Prereqs..."
+sudo pacman -S --needed git base-devel xorg-xfd ttf-bigblueterminal-nerd alacritty starship rofi thunar eza
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo ""
 echo "Switching to fish shell..."
 chsh -s /usr/bin/fish
@@ -14,8 +15,17 @@ echo "Applying Default Themes & Configs..."
 cp -R .config ~/
 cp .xinitrc ~/
 echo ""
-echo "Installing extras..."
 mkdir -p ~/.src
+echo "Installing YaY"
+cd ~/src
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+echo ""
+echo "Installing PreReqs from AUR..."
+yay -S nordic-theme nordzy-icon-theme nordzy-cursors-theme librewolf-bin light
+echo ""
+echo "Installing extras..."
 cd ~/.src
 echo "Building rdir..."
 git clone https://github.com/sigvaldr/rdir.git
